d372335eb950fb43122d7d7130e08454db0150a0
MJ <mahalungguj@gmail.com>
2023-01-21 17:12:34 -0800

    move WCB work on saperate folder

--- WCB/src/playground/cwb/CalculateInterestTest.java
+++ WCB/src/playground/cwb/CalculateInterestTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 public class CalculateInterestTest {
